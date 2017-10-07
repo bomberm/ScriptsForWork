2edba04b5cb33ad47bba4832a67641507e724813
Mars Bomber <bomberm@oregonstate.edu>
2017-10-07 14:34:16 -0700

    Adding twitter script and example docs

---
+++ TwitterScript.py
@@ -0,0 +1,23 @@
+import sys
+import os
+import re
+
+#Retrieve info from twitter
+with open(sys.argv[1], 'r') as f:
+  for line in f:
+		os.system('wget -O - '+line.rstrip()+' | grep "      <title>" >> tempFile')
+
+#Create output file
+output = open(sys.argv[1]+"_Results", 'w+')
+
+#Clean it up
+with open('tempFile', 'r') as f:
+	for line in f:
+		tempLine = line.strip()
+		tempLine=tempLine[7:]
+		tempLine=tempLine[:-14]
+		tempLine = re.sub(" on Twitter","",tempLine)
+		tempLine = re.sub("&quot;","", tempLine)
+		output.write(tempLine+"\n")
+output.close()
+os.system('rm tempFile')
