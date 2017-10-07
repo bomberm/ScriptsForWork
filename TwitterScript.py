15657d3853b2f30e6ea14e66fb22b31229797014
Mars Bomber <bomberm@oregonstate.edu>
2017-10-07 14:39:18 -0700

    Added error check to script

--- TwitterScript.py
+++ TwitterScript.py
@@ -2,22 +2,27 @@ import sys
 import os
 import re
 
+try:
+	sys.argv[1]
+except IndexError:
+	print "Usage: python "+sys.argv[0]+" <filename>"
+else:
 #Retrieve info from twitter
-with open(sys.argv[1], 'r') as f:
-  for line in f:
-		os.system('wget -O - '+line.rstrip()+' | grep "      <title>" >> tempFile')
+	with open(sys.argv[1], 'r') as f:
+		for line in f:
+			os.system('wget -O - '+line.rstrip()+' | grep "      <title>" >> tempFile')
 
 #Create output file
-output = open(sys.argv[1]+"_Results", 'w+')
+	output = open(sys.argv[1]+"_Results", 'w+')
 
 #Clean it up
-with open('tempFile', 'r') as f:
-	for line in f:
-		tempLine = line.strip()
-		tempLine=tempLine[7:]
-		tempLine=tempLine[:-14]
-		tempLine = re.sub(" on Twitter","",tempLine)
-		tempLine = re.sub("&quot;","", tempLine)
-		output.write(tempLine+"\n")
-output.close()
-os.system('rm tempFile')
+	with open('tempFile', 'r') as f:
+		for line in f:
+			tempLine = line.strip()
+			tempLine=tempLine[7:]
+			tempLine=tempLine[:-14]
+			tempLine = re.sub(" on Twitter","",tempLine)
+			tempLine = re.sub("&quot;","", tempLine)
+			output.write(tempLine+"\n")
+	output.close()
+	os.system('rm tempFile')
